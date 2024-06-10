17475c2d10ee77c9d0069339cd9a4b7cf45e1cb6
Lavizord <63145101+Lavizord@users.noreply.github.com>
2024-06-10 11:34:09 +0100

    feat: PostGame endpoint returns correct data.
    - Deleted unused files;
    - Split the GamePostTests.CreateSingleGame into two:
    -- CreateSingleGame_ReturnsCreated() - To Test status code.
    -- CreateSingleGame_ReturnsCorrectData() - To test created data.

--- Api.Tests/Tests/Games/GamePostTests.cs
+++ Api.Tests/Tests/Games/GamePostTests.cs
@@ -9,7 +9,7 @@ namespace Api.Tests
     {
 
         [Fact]
-        public async Task CreateSingleGame()
+        public async Task CreateSingleGame_ReturnsCreated()
         {
             await using var application = new WebApplicationFactory<Program>();
 
@@ -20,9 +20,53 @@ namespace Api.Tests
                 GameName = "UnitTestName",
                 GameDescription = "This is a test game description."
             });
-        
             Assert.Equal(HttpStatusCode.Created, result.StatusCode);
-            // TODO: Acert that returned data was the same as the correct one?
+        }
+
+        [Fact]
+        public async Task CreateSingleGame_ReturnsCorrectData()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            var client = application.CreateClient();
+
+            // Create the game DTO to send in the POST request
+            var gameToCreate = new GameDto
+            {
+                GameName = "GameTest",
+                GameDescription = "This is a test game description.",
+                MinPlayers = 2,
+                MaxPlayers = 6,
+                AverageDuration = 90,
+                MatchesCount = 0
+            };
+
+            // Send the POST request
+            var result = await client.PostAsJsonAsync("/game", gameToCreate);
+
+            // Read and deserialize the response content
+            var jsonResponseString = await result.Content.ReadAsStringAsync();
+            var createdGame = Newtonsoft.Json.JsonConvert.DeserializeObject<GameDto>(jsonResponseString);
+
+            // Create the expected game DTO with the expected Id
+            var expectedGame = new GameDto
+            {
+                Id = createdGame.Id,  // Assume the Id is set by the server and is not known beforehand
+                GameName = "GameTest",
+                GameDescription = "This is a test game description.",
+                MinPlayers = 2,
+                MaxPlayers = 6,
+                AverageDuration = 90,
+                MatchesCount = 0
+            };
+
+            // Assert that the created game matches the expected game
+            Assert.Equal(expectedGame.Id, createdGame.Id);
+            Assert.Equal(expectedGame.GameName, createdGame.GameName);
+            Assert.Equal(expectedGame.GameDescription, createdGame.GameDescription);
+            Assert.Equal(expectedGame.MinPlayers, createdGame.MinPlayers);
+            Assert.Equal(expectedGame.MaxPlayers, createdGame.MaxPlayers);
+            Assert.Equal(expectedGame.AverageDuration, createdGame.AverageDuration);
+            Assert.Equal(expectedGame.MatchesCount, createdGame.MatchesCount);
         }
     }
 }
